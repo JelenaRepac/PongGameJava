483faf302c8d6cb2570c851372990b972ef31ff5
jelena <repac01jelena@gmail.com>
2023-03-23 16:33:00 +0100

    Moving the paddles

--- src/ponggame/GamePanel.java
+++ src/ponggame/GamePanel.java
@@ -88,10 +88,12 @@ public class GamePanel extends JPanel implements Runnable {
 	
 	public class AL extends KeyAdapter{
 		public void keyPressed(KeyEvent e) {
-			
+			paddle1.keyPressed(e);
+			paddle2.keyPressed(e);
 		}
 		public void keyRealesd(KeyEvent e) {
-			
+			paddle1.keyReleased(e);
+			paddle2.keyReleased(e);
 		}
 	}
 }
